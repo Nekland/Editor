138d4cd7342c9005133643b02db93bd4caac21aa
Nek <nek.dev@gmail.com>
2013-09-18 22:50:00 +0200

    Added method getName to modules

--- src/modules.js
+++ src/modules.js
@@ -15,6 +15,7 @@
  *      - getTemplateBefore: return a string that will be insert before the editor
  *      - getTemplateAfter:  return a string that will be insert after the editor
  *      - addEvent:          simply add events on some elements of the template
+ *      - getName:           return a string witch represent the module
  */
 window.nekland.Editor.modules = [];
 
@@ -25,11 +26,15 @@ window.nekland.Editor.modules = [];
 window.nekland.Editor.prototype.checkModules = function() {
     var _i, _len;
 
+    /**
+     * Check if the module implements the needeed methods
+     */
     function checkModule (module) {
         if (
             typeof this.modules[i]['getTemplateBefore'] != 'function' || 
-            typeof this.modules[i]['getTemplateAfter'] != 'function' || 
-            typeof this.modules[i]['addEvents'] != 'function'
+            typeof this.modules[i]['getTemplateAfter']  != 'function' || 
+            typeof this.modules[i]['addEvents']         != 'function' ||
+            typeof this.modules[i]['getName']           != 'function'
         ) {
             return false;
         }
@@ -37,7 +42,9 @@ window.nekland.Editor.prototype.checkModules = function() {
 
     for (_i = 0, _len = this.modules.length; _i < _len; i++) {
         if (!checkModule(this.modules[i])) {
-            throw "A module does\'t work. Check if it implements all needed methods.";
+            var name = this.modules[i].getName ? this.modules[i].getName : this.modules[i].toString();
+
+            throw "A module does\'t work. Check if the following module implements all needed methods." + name;
         }
         this.modules[i].translate = this.translate;
     }
