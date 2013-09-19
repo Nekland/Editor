c4fe5996dc1f4f3e6c13b5aaa9b7821c32bd336e
Nek <nek.dev@gmail.com>
2013-09-19 12:17:17 +0200

    collapsed basic buttons

--- src/modules/basic-module.js
+++ src/modules/basic-module.js
@@ -14,12 +14,14 @@
 
     basicModule.prototype.getTemplateBefore = function() {
         var tpl;
-        tpl = '<button type="button" class="btn btn-default nekland-editor-command" data-editor-module="basic" data-editor-command="bold"><b>' + this.translator.translate('bold', {
+        tpl = '<div class="btn-group">';
+        tpl += '<button type="button" class="btn btn-default nekland-editor-command" data-editor-module="basic" data-editor-command="bold"><b>' + this.translator.translate('bold', {
             ucfirst: true
         }) + '</b></button>';
-        return tpl += '<button type="button" class="btn btn-default nekland-editor-command" data-editor-module="basic" data-editor-command="italic"><i>' + this.translator.translate('italic', {
+        tpl += '<button type="button" class="btn btn-default nekland-editor-command" data-editor-module="basic" data-editor-command="italic"><i>' + this.translator.translate('italic', {
             ucfirst: true
         }) + '</i></button>';
+        return tpl +='</div>'
     };
 
     basicModule.prototype.execute          = function ($button) {
