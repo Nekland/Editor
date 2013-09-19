dbbcf5a88c6399035bc6768f570873609407ddae
Nek <nek.dev@gmail.com>
2013-09-19 02:23:58 +0200

    Added the famous nekland editor translator O/

--- src/translation.js
+++ src/translation.js
@@ -7,18 +7,45 @@
  * on the root directory of this project
  */
 
-window.nekland.Editor.prototype.translate = function(str, options) {
-    var res;
-    if (options == null) {
-        options = {};
-    }
-    if (this.translations[str] != null) {
-        res = this.translations[str];
-    } else {
-        throw new Error('Translation missing');
-    }
-    if (options.ucfirst != null) {
-        res = res.charAt(0).toUpperCase() + res.slice(1);
-    }
-    return res;
+/**
+ * Return an tralator object (with the method "translate" and translations inside)
+ */
+window.nekland.Editor.prototype.getTranslator = function () {
+
+    // Defining the translator class
+    var translator = function () {
+        this.translations = window.nekland.lang.editor[this.settings.lang];
+    };
+
+    /**
+     * Translate a string
+     *
+     * @param str     A string: the key of the translation
+     * @param options An object of options that allow transformation of translations
+     */
+    translator.prototype.translate = function(str, options) {
+        var res;
+
+        // Default value for options is {}
+        if (options == null) {
+            options = {};
+        }
+
+        // Check if the translation exists
+        if (this.translations[str] != null) {
+            res = this.translations[str];
+        } else {
+            throw new Error('Translation missing');
+        }
+
+        // Execute options
+        if (options.ucfirst != null) {
+            res = res.charAt(0).toUpperCase() + res.slice(1);
+        }
+
+        return res;
+    };
+
+
+    return new translator();
 };
